4369a44035025ab1e5cb70ec21618fc12702375b
unknown <jonikarell1@gmail.com>
2024-08-14 14:32:09 +0700

    edit multer

--- middleware/multerProduct.js
+++ middleware/multerProduct.js
@@ -2,26 +2,26 @@ const multer = require('multer');
 const path = require('path');
 
 //local
-// const storage = multer.diskStorage({
-//     destination: (req, file, callback) => {
-//         callback(null, path.join(__dirname, '../../../../public/uploads/product'));
-//     },
-//     filename: (req, file, callback) => {
-//         const fileName = file.originalname.toLowerCase().split(' ').join('-');
-//         callback(null, Date.now() + '-' + fileName);
-//     },
-// });
-
-//vercel
 const storage = multer.diskStorage({
     destination: (req, file, callback) => {
-        callback(null, path.join(process.cwd(), '../../../../../../public/uploads/product'));
+        callback(null, path.join(__dirname, '/uploads/product'));
     },
     filename: (req, file, callback) => {
         const fileName = file.originalname.toLowerCase().split(' ').join('-');
         callback(null, Date.now() + '-' + fileName);
     },
 });
+
+//vercel
+// const storage = multer.diskStorage({
+//     destination: (req, file, callback) => {
+//         callback(null, path.join(process.cwd(), '/uploads/product'));
+//     },
+//     filename: (req, file, callback) => {
+//         const fileName = file.originalname.toLowerCase().split(' ').join('-');
+//         callback(null, Date.now() + '-' + fileName);
+//     },
+// });
 
 const productUpload = multer({ storage });
 
