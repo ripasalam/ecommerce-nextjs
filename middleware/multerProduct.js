4d94c0253f1a4b6ec509886fed8762363791e36f
unknown <jonikarell1@gmail.com>
2024-08-14 10:43:51 +0700

    edit multer 2

--- middleware/multerProduct.js
+++ middleware/multerProduct.js
@@ -15,7 +15,7 @@ const path = require('path');
 //vercel
 const storage = multer.diskStorage({
     destination: (req, file, callback) => {
-        callback(null, path.join(__dirname, '../../public/uploads/product'));
+        callback(null, path.join(__dirname, '../../../../../../public/uploads/product'));
     },
     filename: (req, file, callback) => {
         const fileName = file.originalname.toLowerCase().split(' ').join('-');
